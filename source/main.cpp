246b6a9f223fba942e45c96d4a520403e4522b0b
Naxel <46362288+xNaxelx@users.noreply.github.com>
2023-01-30 21:31:11 +0200

    Add main cycle

--- source/main.cpp
+++ source/main.cpp
@@ -1,41 +1,110 @@
 ﻿#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#include <SDL_mixer.h>
 #include <stdio.h>
+#include <memory>
+
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
-int main(int argc, char* args[])
+SDL_Window* window;
+std::shared_ptr<SDL_Renderer> renderer; 
+TTF_Font* font = NULL;
+
+bool Init()
 {
-	SDL_Window* window = NULL;
-
-	SDL_Surface* screenSurface = NULL;
-
-	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	if (SDL_Init(SDL_INIT_TIMER | SDL_INIT_AUDIO | SDL_INIT_VIDEO | SDL_INIT_EVENTS) < 0)
 	{
-		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+		printf("SDL could not initialize! SDL Error: %s\n", SDL_GetError());
+		return false;
 	}
-	else
+	if (!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1"))
 	{
-		window = SDL_CreateWindow("SDL work test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-		if (window == NULL)
-		{
-			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
-		}
-		else
-		{
-			screenSurface = SDL_GetWindowSurface(window);
-
-			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
-
-			SDL_UpdateWindowSurface(window);
-
-			SDL_Event e; bool quit = false; while (quit == false) { while (SDL_PollEvent(&e)) { if (e.type == SDL_QUIT) quit = true; } }
-		}
+		printf("Warning: Linear texture filtering not enabled!\n");
 	}
+
+	window = SDL_CreateWindow("BlackJack", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+	if (window == NULL)
+	{
+		printf("Window could not be created! SDL Error: %s\n", SDL_GetError());
+		return false;
+	}
+
+	renderer.reset(SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC), [](SDL_Renderer* renderer) { SDL_DestroyRenderer(renderer); });
+	if (renderer == NULL)
+	{
+		printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
+		return false;
+	}
+	SDL_SetRenderDrawColor(renderer.get(), 0x0, 0x0, 0x0, 0xFF);
+
+	if (!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG))
+	{
+		printf("SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError());
+		return false;
+	}
+
+	if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0)
+	{
+		printf("SDL_mixer could not initialize! SDL_mixer Error: %s\n", Mix_GetError());
+		return false;
+	}
+
+	if (TTF_Init() == -1)
+	{
+		printf("SDL_ttf could not initialize! SDL_ttf Error: %s\n", TTF_GetError());
+		return false;
+	}
+
+	return true;
+}
+
+void Close()
+{
+	//TTF_CloseFont(font);
+	font = NULL;
 
 	SDL_DestroyWindow(window);
+	window = NULL;
+	renderer.~shared_ptr();	
+	renderer = NULL;
 
+	TTF_Quit();
+	IMG_Quit();
 	SDL_Quit();
+}
 
+int main(int argc, char* args[])
+{
+	if (!Init())
+	{
+		printf("Failed to initialize!\n");
+
+		Close();
+
+		return -1;
+	}
+	
+	bool quit = false;
+	SDL_Event event;
+
+	while (!quit)
+	{
+		while (SDL_PollEvent(&event) != 0)
+		{
+			if (event.type == SDL_QUIT) { quit = true; }
+		}
+
+		SDL_SetRenderDrawColor(renderer.get(), 0x0, 0x0, 0x0, 0xFF);
+		SDL_RenderClear(renderer.get());
+
+		SDL_RenderPresent(renderer.get());
+	}	
+
+
+	Close();
+	
 	return 0;
 }
